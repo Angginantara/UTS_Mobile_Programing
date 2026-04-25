ea95cd2282062c7d7414daf2568c1a906925f647
Angginantara <angginantaraputra@gmail.com>
2026-04-25 23:35:26 +0800

    mengubah lib main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'home.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Diva APP',
+      title: 'Diva App',
       home: LoginPage(),
     );
   }
@@ -23,46 +24,35 @@ class LoginPage extends StatelessWidget {
     if (username.text == "admin" && password.text == "1234") {
       Navigator.push(
         context,
-        MaterialPageRoute(builder: (context) => DashboardPage()),
+        MaterialPageRoute(builder: (context) => HomePage()),
       );
     } else {
-      ScaffoldMessenger.of(
-        context,
-      ).showSnackBar(SnackBar(content: Text("Username / Password salah")));
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Username / Password salah")),
+      );
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 70, 8, 162),
+      backgroundColor: Colors.purple,
       body: Center(
         child: Container(
           width: 320,
           padding: EdgeInsets.all(20),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(15),
-          ),
+          color: Colors.white,
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              // LOGO
               CircleAvatar(
                 radius: 45,
-                backgroundColor: Colors.transparent,
                 backgroundImage: AssetImage('assets/logo.jpeg'),
               ),
-
               SizedBox(height: 15),
-
-              Text(
-                "Diva App",
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-
+              Text("Diva App",
+                  style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold)),
               SizedBox(height: 20),
-
               TextField(
                 controller: username,
                 decoration: InputDecoration(
@@ -70,9 +60,7 @@ class LoginPage extends StatelessWidget {
                   border: OutlineInputBorder(),
                 ),
               ),
-
               SizedBox(height: 10),
-
               TextField(
                 controller: password,
                 obscureText: true,
@@ -81,169 +69,13 @@ class LoginPage extends StatelessWidget {
                   border: OutlineInputBorder(),
                 ),
               ),
-
-              SizedBox(height: 5),
-
-              Align(
-                alignment: Alignment.centerRight,
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => ForgotPasswordPage(),
-                      ),
-                    );
-                  },
-                  child: Text(
-                    "Forgot Password?",
-                    style: TextStyle(color: Colors.black),
-                  ),
-                ),
-              ),
-
-              SizedBox(height: 10),
-
+              SizedBox(height: 20),
               ElevatedButton(
                 onPressed: () => login(context),
-                style: ElevatedButton.styleFrom(
-                  minimumSize: Size(double.infinity, 45),
-                ),
-                child: Text("Login", style: TextStyle(color: Colors.black)),
-              ),
-
-              SizedBox(height: 15),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Belum punya akun? "),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => RegisterPage()),
-                      );
-                    },
-                    child: Text(
-                      "Daftar",
-                      style: TextStyle(
-                        color: Colors.blue,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-
-              SizedBox(height: 10),
-
-              Text(
-                "Version 1.0.0",
-                style: TextStyle(fontSize: 12, color: Colors.grey),
-              ),
+                child: Text("Login"),
+              )
             ],
           ),
-        ),
-      ),
-    );
-  }
-}
-
-// DASHBOARD
-class DashboardPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Dashboard"),
-        backgroundColor: Color.fromARGB(255, 37, 30, 233),
-      ),
-      body: Center(
-        child: Text(
-          "Selamat Datang di Diva App 💖",
-          style: TextStyle(fontSize: 24),
-        ),
-      ),
-    );
-  }
-}
-
-// FORGOT PASSWORD
-class ForgotPasswordPage extends StatelessWidget {
-  final TextEditingController email = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Forgot Password")),
-      body: Padding(
-        padding: EdgeInsets.all(20),
-        child: Column(
-          children: [
-            SizedBox(height: 20),
-            Text("Masukkan Email Anda"),
-            SizedBox(height: 20),
-            TextField(
-              controller: email,
-              decoration: InputDecoration(
-                hintText: "Email",
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                ScaffoldMessenger.of(
-                  context,
-                ).showSnackBar(SnackBar(content: Text("Link reset dikirim")));
-              },
-              child: Text("Kirim"),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-// REGISTER PAGE
-class RegisterPage extends StatelessWidget {
-  final TextEditingController username = TextEditingController();
-  final TextEditingController password = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Daftar Akun")),
-      body: Padding(
-        padding: EdgeInsets.all(20),
-        child: Column(
-          children: [
-            TextField(
-              controller: username,
-              decoration: InputDecoration(
-                hintText: "Username",
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 10),
-            TextField(
-              controller: password,
-              obscureText: true,
-              decoration: InputDecoration(
-                hintText: "Password",
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text("Daftar"),
-            ),
-          ],
         ),
       ),
     );
