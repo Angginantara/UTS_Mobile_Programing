160a85cc7156f5a04503552e928111e6e7ec0420
Angginantara <angginantaraputra@gmail.com>
2026-04-26 15:49:44 +0800

    mengubah lib forgot_password

--- lib/forgot_password.dart
+++ lib/forgot_password.dart
@@ -1,55 +1,36 @@
 import 'package:flutter/material.dart';
 
 class ForgotPasswordPage extends StatelessWidget {
-  const ForgotPasswordPage({super.key});
+  ForgotPasswordPage();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.purple,
       appBar: AppBar(
         title: Text("Forgot Password"),
         backgroundColor: Colors.purple,
       ),
-      body: Center(
-        child: Container(
-          padding: EdgeInsets.all(20),
-          margin: EdgeInsets.all(20),
-          color: Colors.white,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
 
-              Text(
-                "Reset Password",
-                style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: "Masukkan Email",
               ),
+            ),
 
-              SizedBox(height: 20),
+            SizedBox(height: 20),
 
-              TextField(
-                decoration: InputDecoration(
-                  labelText: "Masukkan Email",
-                  border: OutlineInputBorder(),
-                ),
-              ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("Kirim"),
+            )
 
-              SizedBox(height: 20),
-
-              ElevatedButton(
-                onPressed: () {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text("Link reset dikirim ke email")),
-                  );
-                },
-                child: Text("Kirim"),
-              )
-
-            ],
-          ),
+          ],
         ),
       ),
     );
